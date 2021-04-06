c85e74ac2c6da93cf267930f002e0be2142d936d
yii.yang <47237767+LikangZhou@users.noreply.github.com>
2021-04-06 18:18:41 +0800

    1

--- 打包/sql.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table ldtask...
-set feedback off
-set define off
-insert into ldtask (TASKCODE, TASKNAME, TASKCONTROLLERPATH, TASKSTATUS, TASKRUNTIME, TASKDESC, TASKMODIFYDATE, TASKMODIFYTIME)
-values ('022', 'inshDebitILITask', '/ExcelReportController/inshDebitILI.do', 'N', '每天', '投连追加INSH扣款跑批', null, null);
-
-prompt Done.
