a6c41a93b1319ab1de7b5fe2b35d69bfb9aeeba6
yii.yang <47237767+LikangZhou@users.noreply.github.com>
2021-05-14 16:39:09 +0800

    1

--- TheOthers/TYF/tyf.sql
+++
@@ -1,7 +0,0 @@
-prompt Importing table menu...
-set feedback off
-set define off
-insert into menu (ID, PAR_UID, MENU_NAME, MENU_DESC, MENU_STATUS, MENU_LINK, MENU_INDEX, T_S_UID, MEDDLE_MENU, MEDDLE_MENU_NAME, CRT_TIME, UPD_TIME, SUB_MENU_NAME, SUB_MENU)
-values ('04def291-2e6c-43e7-b0f2-fe65eb490ca1', null, '测试单维护页面', '1', null, '/application/testsingle/jsp/testsingle.jsp', 0, null, 8, '测试单维护页面', null, null, '银保业务', 1);
-
-prompt Done.
